861853b5031d1ed97ea9408d4618cebe4c25b04d
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-04-01 14:27:24 +0000

    Moved files to trunk to respect typo3xdev structure
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/advancedform/trunk@8762 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "advancedform"
+#
+# Auto generated 18-03-2008 10:30
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Advanced FORM cObj',
+	'description' => 'Ideas for replacement of the standard FORM cObj with an enhanced engine. Includes utility classes to build forms.',
+	'category' => 'fe',
+	'author' => 'Francois Suter (Cobweb)',
+	'author_email' => 'support@cobweb.ch',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'experimental',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"fa73";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"ce4a";s:14:"ext_tables.php";s:4:"bf2a";s:16:"locallang_db.xml";s:4:"9093";s:42:"lib/class.tx_advancedform_formelements.php";s:4:"cf97";s:40:"lib/class.tx_advancedform_tcatoforms.php";s:4:"4bdb";s:33:"pi1/class.tx_advancedform_pi1.php";s:4:"9abe";s:17:"pi1/locallang.xml";s:4:"0aa5";}',
+	'suggests' => array(
+	),
+);
+
+?>
