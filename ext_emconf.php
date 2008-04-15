1587d4f3404592d8f69756d66a731ae3a912a1fa
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-04-15 10:37:47 +0000

    Released to TER as version 0.0.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/advancedform/trunk@8913 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "advancedform"
 #
-# Auto generated 18-03-2008 10:30
+# Auto generated 15-04-2008 12:37
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.2',
+	'version' => '0.0.3',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"fa73";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"ce4a";s:14:"ext_tables.php";s:4:"bf2a";s:16:"locallang_db.xml";s:4:"9093";s:42:"lib/class.tx_advancedform_formelements.php";s:4:"cf97";s:40:"lib/class.tx_advancedform_tcatoforms.php";s:4:"4bdb";s:33:"pi1/class.tx_advancedform_pi1.php";s:4:"9abe";s:17:"pi1/locallang.xml";s:4:"0aa5";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"fa73";s:10:"README.txt";s:4:"6ad6";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"7ee8";s:14:"ext_tables.php";s:4:"bf2a";s:16:"locallang_db.xml";s:4:"9093";s:42:"lib/class.tx_advancedform_formelements.php";s:4:"4d1c";s:40:"lib/class.tx_advancedform_helperbase.php";s:4:"9767";s:40:"lib/class.tx_advancedform_tcatoforms.php";s:4:"5f14";s:33:"pi1/class.tx_advancedform_pi1.php";s:4:"aa91";s:17:"pi1/locallang.xml";s:4:"0aa5";}',
 	'suggests' => array(
 	),
 );
